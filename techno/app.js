0b0d934d6d62eeb7201b75d32282b5a9ce5d3cfd
MarcosCruzz <marcosant2306@gmail.com>
2023-04-03 10:42:28 -0400

    commit inicial

---
+++ techno/app.js
@@ -0,0 +1,94 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    produtos: [],
+    produto: false,
+    mensagemAlerta: "Item adicionado",
+    carrinho: [],
+    carrinhoAtivo: false,
+    alertaAtivo: false,
+  },
+
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-Br", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
+  },
+
+  computed: {
+    carrinhoTotal() {
+      let total = 0;
+      if (this.carrinho.length) {
+        this.carrinho.forEach((item) => {
+          total += item.preco;
+        });
+      }
+      return total;
+    },
+  },
+
+  methods: {
+    fetchProdutos() {
+      fetch("./api/produtos.json")
+        .then((r) => r.json())
+        .then((r) => {
+          this.produtos = r;
+        });
+    },
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((r) => r.json())
+        .then((r) => {
+          this.produto = r;
+        });
+    },
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+      });
+    },
+
+    clickForaCarrinho({ target, currentTarget }) {
+      if (target === currentTarget) this.carrinhoAtivo = false;
+    },
+    adicionarItem() {
+      this.produto.estoque--;
+      const { id, nome, preco } = this.produto;
+      this.carrinho.push({ id, nome, preco });
+      this.alerta(`${nome} adicionado ao carrinho.`);
+    },
+
+    removerItem(index) {
+      this.carrinho.splice(index, 1);
+    },
+
+    checarLocalStorage() {
+      if (window.localStorage.carrinho) {
+        this.carrinho = JSON.parse(window.localStorage.carrinho);
+      }
+    },
+
+    alerta(mensagem) {
+      this.mensagemAlerta = mensagem;
+      this.alertaAtivo = true;
+      setTimeout(() => {
+        this.alertaAtivo = false;
+      }, 1500);
+    },
+  },
+
+  watch: {
+    carrinho() {
+      window.localStorage.carrinho = JSON.stringify(this.carrinho);
+    },
+  },
+
+  created() {
+    this.fetchProdutos();
+    this.checarLocalStorage();
+  },
+});
